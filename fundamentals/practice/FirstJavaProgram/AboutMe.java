b5ed628155f140e3b56157d4c8acb66848110e5a
hope-crichlow <hopecrichlow@gmail.com>
2021-08-02 14:01:25 -0600

    Initial Commit

---
+++ fundamentals/practice/FirstJavaProgram/AboutMe.java
@@ -0,0 +1,7 @@
+public class AboutMe {
+    public static void main(String[] args){
+        System.out.println("My name is Hadiya");
+        System.out.println("I am 31 years old");
+        System.out.println("My hometown is Bridgetown, Barbados");
+    }
+}
