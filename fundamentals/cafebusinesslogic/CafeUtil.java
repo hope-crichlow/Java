e3b49711eab6cb2622bf4f5a7f767e6e19701417
hope-crichlow <hopecrichlow@gmail.com>
2021-08-04 15:40:13 -0700

    Notes

---
+++ fundamentals/cafebusinesslogic/CafeUtil.java
@@ -0,0 +1,39 @@
+public class CafeUtil {
+
+//(void) militaryHoursTest
+// Write a method that prints all the numbers from 0 to 23.
+    public void militaryHoursTest(){
+
+    }
+
+
+// (void) coffeeNotification
+// Starting at 0 and ending after 23, for every hour in the day, print out each hour as a string like so: "It's 4:00 hours, do you need a coffee break?". Hint: Use String.format() to put the hour integer in the string. Note: you do not need to include any leading zeros.
+
+
+// (void) specialsAlert
+// Starting at 6 and ending at 15, iterate through each number, representing an hour, if the hour is a multiple of 3, print "Buy one get one free for the next 15 minutes!" Otherwise, print "Stay tuned for deals announced throughout the day."
+
+
+// (int) leadsAtDay10
+// Starting with 1, sum together all the numbers from 1 to 10. This will calculate how many leads a sales rep needs to have after 10 days to get the reward for that period. Return the sum after it has been calculated.
+
+
+// (void) displayMenu
+// Given an array of menu item names (strings), iterate through the array and print all the menu items in the array.
+
+
+// (double) getOrderTotal
+// Given an array of the prices from the items in a customer order, iterate over the array to sum each item price and to return the total
+
+
+// (double) getHighestPrice
+// Given an array of all the item prices, find the highest price for any item
+
+
+// (ArrayList<String>) getRaffleWinners
+// In your method, create an empty array,  winners . Iterate over a given array of customers and add every 4th customer to the winners array. Return the winners array, when you've added all the winners.
+
+
+
+}
