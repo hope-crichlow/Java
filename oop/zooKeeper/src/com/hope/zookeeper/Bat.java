f92b58a60f37d202036cc9f985b5ea1edfb50b72
hope-crichlow <hopecrichlow@gmail.com>
2021-08-05 22:24:20 -0700

    Bat methods built out

--- oop/zooKeeper/src/com/hope/zookeeper/Bat.java
+++ oop/zooKeeper/src/com/hope/zookeeper/Bat.java
@@ -1,9 +1,29 @@
 package com.hope.zookeeper;
 
-public class Bat {
+public class Bat extends Mammal{
 
 	public Bat() {
-		// TODO Auto-generated constructor stub
+	
+		this.energyLevel = 300;
 	}
-
+	
+//	fly()
+	public void fly() {
+		System.out.println("Bat has taken to the sky! Energy level has been decreased by 50");
+		this.energyLevel -= 50;
+		displayEnergy();
+	}
+//	eatHumans()
+	public void eatHumans() {
+		System.out.println("Just a nibble... Energy level has been increased by 25");
+		this.energyLevel += 25;
+		displayEnergy();
+		
+	}
+//	attackTown()
+	public void attackTown() {
+		System.out.println("*Sounds of Fire and Mayhem* Energy level has been decreased by 100");
+		this.energyLevel -= 100;
+		displayEnergy();
+	}
 }
