99f254a1fe066edf9f9e64e9124f1a4d697b9867
hope-crichlow <hopecrichlow@gmail.com>
2021-08-08 12:33:30 -0700

    Create a BankAccount class

---
+++ oop/bankAccount/src/com/hope/bankaccount/BankAccount.java
@@ -0,0 +1,25 @@
+package com.hope.bankaccount;
+
+public class BankAccount {
+
+//	MEMBER VARIABLES
+//	Attribute: (double) checking balance
+//	Attribute: (double) savings balance.
+//	Class Member: (static) that has the number of accounts created thus far.
+//	Class Member: (static) that tracks the total amount of money stored in every account created.
+	
+//	CONSTRUCTOR METHODS
+//	Increment the account count.
+	
+//	GETTERS AND SETTERS
+//	Users should not be able to set any of the attributes from the class.
+
+//	Getter Method: User's checking account balance.
+//	Getter Method: User's saving account balance.
+//	Getter Method: User's total money from the checking and saving.
+	
+//	OTHER METHODS
+//	Method: Deposit money into either the checking or saving, be sure to add to total amount stored.
+//	Method: Withdraw money from one balance. Do not allow them to withdraw money if there are insufficient funds.
+
+}
---
+++ oop/bankAccount/src/com/hope/bankaccount/BankAccountTest.java
@@ -0,0 +1,10 @@
+package com.hope.bankaccount;
+
+public class BankAccountTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
