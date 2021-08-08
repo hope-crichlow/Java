1ea78f1ddbbaede0a9be5eaed638908f5af759a7
hope-crichlow <hopecrichlow@gmail.com>
2021-08-08 16:25:39 -0700

    Initial commit

---
+++ oop/phoneAssignment/src/com/hope/phone/Ringable.java
@@ -0,0 +1,9 @@
+package com.hope.phone;
+
+public interface Ringable {
+//	Create a Ringable interface that contains the following methods:
+
+//ring(): This method returns a String.
+
+//unlock(): This method returns a String.
+}
