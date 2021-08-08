5c760b2ddcce1dba7cf2aeb5649aa4e9ad76be10
hope-crichlow <hopecrichlow@gmail.com>
2021-08-08 16:29:18 -0700

    Interface methods added

--- oop/phoneAssignment/src/com/hope/phone/Ringable.java
+++ oop/phoneAssignment/src/com/hope/phone/Ringable.java
@@ -4,6 +4,11 @@ public interface Ringable {
 //	Create a Ringable interface that contains the following methods:
 
 //ring(): This method returns a String.
-
+	default String ring() {
+		return "Ringgg";
+	}
 //unlock(): This method returns a String.
+	default String unlock() {
+		return "Phone has been unlocked";
+	}
 }
