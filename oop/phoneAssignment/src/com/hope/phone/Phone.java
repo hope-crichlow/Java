321ef7f45094379d2d9d290b8d2e8a1d97461ef6
hope-crichlow <hopecrichlow@gmail.com>
2021-08-08 16:56:39 -0700

    Getters and setters added

--- oop/phoneAssignment/src/com/hope/phone/Phone.java
+++ oop/phoneAssignment/src/com/hope/phone/Phone.java
@@ -5,13 +5,52 @@ public abstract class Phone {
     private int batteryPercentage;
     private String carrier;
     private String ringTone;
+    
     public Phone(String versionNumber, int batteryPercentage, String carrier, String ringTone){
         this.versionNumber = versionNumber;
         this.batteryPercentage = batteryPercentage;
         this.carrier = carrier;
         this.ringTone = ringTone;
     }
-    // abstract method. This method will be implemented by the subclasses
+    
+//  Abstract Method. This method will be implemented by the subclasses
     public abstract void displayInfo();
-    // getters and setters removed for brevity. Please implement them yourself
+    
+//  Getters and Setters
+
+    public String getVersionNumber() {
+    	return this.versionNumber;
+    }
+    
+    public void setVersionNumber(String newVersionNumber) {
+    	this.versionNumber = newVersionNumber;
+    }
+    
+    
+    public int getBatteryPercentage() {
+    	return this.batteryPercentage;
+    }
+    
+    public void setBatteryPercentage(int newBatteryPercentage) {
+    	this.batteryPercentage = newBatteryPercentage;
+    }
+    
+    
+    public String getCarrier() {
+    	return this.carrier;
+    }
+    
+    public void setCarrier(String newCarrier) {
+    	this.carrier = newCarrier;
+    }
+    
+    
+    public String getRingTone() {
+    	return this.ringTone;
+    }
+    
+    public void setRingTone(String newRingTone) {
+    	this.ringTone = newRingTone;
+    }
+    
 }
