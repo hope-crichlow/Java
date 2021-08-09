123c8631e2d6d484f2fed3c05d2d26a3a7b84d1d
hope-crichlow <hopecrichlow@gmail.com>
2021-08-09 10:04:41 -0700

    Formatting

--- spring/helloHuman/HelloHuman/src/main/java/com/hope/hellohuman/HelloHumanApplication.java
+++ spring/helloHuman/HelloHuman/src/main/java/com/hope/hellohuman/HelloHumanApplication.java
@@ -11,6 +11,5 @@ public class HelloHumanApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloHumanApplication.class, args);
 	}
-	
 
 }
--- spring/helloHuman/HelloHuman/src/main/java/com/hope/hellohuman/controllers/HumanController.java
+++ spring/helloHuman/HelloHuman/src/main/java/com/hope/hellohuman/controllers/HumanController.java
@@ -1,23 +1,22 @@
 package com.hope.hellohuman.controllers;
 
 import org.springframework.web.bind.annotation.PathVariable;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
 public class HumanController {
 
 	@RequestMapping("/")
-
 	public String index() { 
+		
 		return "Hello Human!" + "<br><br><br>" + "Welcome to SpringBoot!";
 	}
 
 	@RequestMapping("/{name}")
 	public String viewName(@PathVariable("name") String name) {
 		System.out.println(name);
+		
 		return "Hello " + name + " !" +"<br><br><br>" + "Welcome to SpringBoot!" ;
 	}
 
