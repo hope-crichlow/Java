a390af5506bb83c33a3f1a420834e7f368525de4
hope-crichlow <hopecrichlow@gmail.com>
2021-08-09 07:24:52 -0700

    Tinkering

--- oop/phoneAssignment/src/com/hope/phone/IPhone.java
+++ oop/phoneAssignment/src/com/hope/phone/IPhone.java
@@ -6,7 +6,6 @@ public class IPhone extends Phone implements Ringable {
 	
     public IPhone(String versionNumber, int batteryPercentage, String carrier, String ringTone) {
         super(versionNumber, batteryPercentage, carrier, ringTone);
- 
     }
     
     @Override
