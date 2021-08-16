2974da430a111843e9bfb9f13865b0bc7fb684ff
hope-crichlow <hopecrichlow@gmail.com>
2021-08-16 09:55:58 -0700

    Model setup

--- spring/Languages/src/main/java/com/hope/languages/models/Language.java
+++ spring/Languages/src/main/java/com/hope/languages/models/Language.java
@@ -46,9 +46,76 @@ public class Language {
     private Date updatedAt;
 	
     
- // OTHER METHODS
+    // CONSTRUCTORS
+    	
+    	// EMPTY CONSTRUCTOR
+    public Language() {
+    	
+    }
+    
+    	// FULL CONSTRUCTOR
+	public Language(String name, String creator, String version) {
+		super();
+		this.name = name;
+		this.creator = creator;
+		this.version = version;
+	}
 	
- 		// Updates createdAt
+
+    
+    // GETTERS AND SETTERS
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCreator() {
+		return creator;
+	}
+
+	public void setCreator(String creator) {
+		this.creator = creator;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	
+	// OTHER METHODS
+		// Updates createdAt
      @PrePersist
      protected void onCreate(){
          this.createdAt = new Date();
