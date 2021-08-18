ef2e6b60cc1b6030dab2e355691af01bc0bbf26a
hope-crichlow <hopecrichlow@gmail.com>
2021-08-18 12:41:07 -0700

    Create Dojo functionality working

--- spring/DojosAndNinjas/src/main/java/com/hope/dojosandninjas/controllers/MainController.java
+++ spring/DojosAndNinjas/src/main/java/com/hope/dojosandninjas/controllers/MainController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.hope.dojosandninjas.models.Dojo;
 import com.hope.dojosandninjas.services.MainService;
@@ -20,6 +21,11 @@ public class MainController {
 		return "index.jsp";
 	}
 
+	@PostMapping("/dojo/new")
+	public String createDojo(@ModelAttribute("dojoObj") Dojo filledDojo) {
+		mainServ.saveDojo(filledDojo);
+		return "redirect:/";
+	}
 	@GetMapping("/dojos/new")
 	public String newDojo() {
 		return "redirect:/";
