bce55e0349c49942cca3aae7e19bc99e0b2c8dfd
hope-crichlow <hopecrichlow@gmail.com>
2021-08-18 12:34:28 -0700

    Form on / rendering

--- spring/DojosAndNinjas/src/main/java/com/hope/dojosandninjas/controllers/MainController.java
+++ spring/DojosAndNinjas/src/main/java/com/hope/dojosandninjas/controllers/MainController.java
@@ -1,13 +1,22 @@
 package com.hope.dojosandninjas.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.hope.dojosandninjas.models.Dojo;
+import com.hope.dojosandninjas.services.MainService;
 
 @Controller
 public class MainController {
 
+	
+	@Autowired
+	private MainService mainServ;
+	
 	@GetMapping("/")
-	public String index() {
+	public String index(@ModelAttribute("dojoObj") Dojo emptyDojo) {
 		return "index.jsp";
 	}
 
