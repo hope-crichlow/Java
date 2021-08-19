5bb1b88687bc5966b77cd01227cf88dbd9b733d7
hope-crichlow <hopecrichlow@gmail.com>
2021-08-18 21:03:10 -0700

    Basic service built out

--- spring/ProductsAndCategories/src/main/java/com/hope/productsandcategories/services/MainService.java
+++ spring/ProductsAndCategories/src/main/java/com/hope/productsandcategories/services/MainService.java
@@ -1,8 +1,12 @@
 package com.hope.productsandcategories.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.hope.productsandcategories.models.Category;
+import com.hope.productsandcategories.models.Product;
 import com.hope.productsandcategories.repositories.CategoryRepository;
 import com.hope.productsandcategories.repositories.ProductRepository;
 
@@ -16,8 +20,23 @@ public class MainService {
 	private CategoryRepository catRepo;
 	
 // ------------------- CRUD PRODUCTS ------------------- //
+	
+	public List<Product> allProducts(){
+		return prodRepo.findAll();
+	}
+	
+	public Product save(Product p) {
+		return prodRepo.save(p);
+	}
 // ------------------- CRUD PRODUCTS ------------------- //
 
 // ------------------- CRUD CATEGORIES ------------------- //
+	public List<Category> allCategories(){
+		return catRepo.findAll();
+	}
+	
+	public Category save(Category c) {
+		return catRepo.save(c);
+	}
 // ------------------- CRUD CATEGORIES ------------------- //
 }
